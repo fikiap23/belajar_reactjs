509c0943e661a42f7a4262886edc524980682cae
fikiaprian23 <fikiaprian23@gmail.com>
2023-07-27 10:07:47 +0700

    createElement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2>My First Component</h2>
+//     </div>
+//   )
+// }
 
-function Greeting(){
-    return <h2>My First Component</h2>
+// function Greeting() {
+//   return React.createElement('h2', {}, 'hellow world')
+// }
+
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'hello world')
+  )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting/>);
+root.render(<Greeting />)
